351ddc36c586f8d0752eccc6bfdabc4ec57b896a
bryercowan <cowan224@gmail.com>
2024-09-22 22:17:36 -0500

    cleaned up public, added route api

---
+++ app/api/send-email/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
+
+export async function POST(request: Request) {
+  const { name, email, message } = await request.json();
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  try {
+    await transporter.sendMail({
+      from: {
+        name: "Stack Test Group LLC - Inquiry",
+        address: "contact@stacktestllc.com"
+      },
+      to: "chasityc@stacktestllc.com",
+      subject: `Contact from ${name}`,
+      text: `${message}\n\nFrom: ${name}\nEmail: ${email}`,
+    });
+
+    return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
+  } catch (error) {
+    console.error('Failed to send email:', error);
+    return NextResponse.json({ error: 'Failed to send email' }, { status: 500 });
+  }
+}
